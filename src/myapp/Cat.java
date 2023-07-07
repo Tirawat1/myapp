5e6c573e17abcceb5e3cc61f7ecb636b6fc6213a
Tirawat1 <Tirawat.po@ku.th>
2023-07-07 07:29:16 +0700

    update Cat and add Dog

--- src/myapp/Cat.java
+++ src/myapp/Cat.java
@@ -1,4 +1,13 @@
 package myapp;
 
 public class Cat {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+    public String sayTheyName(){
+        return name;
+    }
 }
